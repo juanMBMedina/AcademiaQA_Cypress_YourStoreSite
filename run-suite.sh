a95f109185b6d86a195be7854a32b2039cc9461b
Juan Miguel <juanmblancom@gmail.com>
2025-04-08 09:03:02 -0500

    Reformat in run-suite.sh for Test Suites

--- run-suite.sh
+++ run-suite.sh
@@ -4,15 +4,23 @@ SUITE=$1
 BROWSER=$2
 
 if [ -z "$SUITE" ] || [ -z "$BROWSER" ]; then
-  echo "Uso: ./run-suite.sh <SUITE> <BROWSER>"
+  echo "Use: ./run-suite.sh <SUITE> <BROWSER>"
   exit 1
 fi
 
-if [ "$SUITE" == "AllSuites" ]; then
+SPECS=$(jq -r --arg suite "$SUITE" '.[$suite] | join(",")' suites.json)
+
+if [ "$SPECS" == "null" ]; then
+  echo "Suite '$SUITE' didn't found in suites.json"
+  exit 1
+fi
+
+if [ -z "$SPECS" ]; then
   npx cypress run \
     --browser "$BROWSER"
 else
   npx cypress run \
-    --spec "cypress/e2e/${SUITE}.cy.js" \
+    --spec "$SPECS" \
     --browser "$BROWSER"
 fi
+
